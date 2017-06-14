9d5520efebb73f1b1dd6a6c2f957a553f5346800
J0ACH <jachym.pesek@gmail.com>
2017-06-14 15:21:15 +0200

    cmake build type

--- cmake/RegPackage.cmake
+++ cmake/RegPackage.cmake
@@ -1,48 +1,74 @@
 MESSAGE(STATUS "RegPackage")
 
+MACRO (TODAY RESULT)
+    IF (WIN32)
+        EXECUTE_PROCESS(COMMAND "cmd" " /C date /T" OUTPUT_VARIABLE ${RESULT})
+        string(REGEX REPLACE "(..)/(..)/..(..).*" "\\1/\\2/\\3" ${RESULT} ${${RESULT}})
+    ELSEIF(UNIX)
+        EXECUTE_PROCESS(COMMAND "date" "+%d/%m/%Y" OUTPUT_VARIABLE ${RESULT})
+        string(REGEX REPLACE "(..)/(..)/..(..).*" "\\1/\\2/\\3" ${RESULT} ${${RESULT}})
+    ELSE (WIN32)
+        MESSAGE(SEND_ERROR "date not implemented")
+        SET(${RESULT} 000000)
+    ENDIF (WIN32)
+ENDMACRO (TODAY)
+
+
+
 FUNCTION(RegPackage PackageName PackageConfig_Dir)
+
+	SET (PKGDIR "${PackageConfig_Dir}/cmake/${CMAKE_CONFIGURATION_TYPES}")
+
+	INCLUDE(CMakePackageConfigHelpers)
+
+	CONFIGURE_PACKAGE_CONFIG_FILE(
+		${CMAKE_SOURCE_DIR}/${PackageName}Config.cmake.in
+		${PKGDIR}/${PackageName}Config.cmake
+		INSTALL_DESTINATION ${CMAKE_INSTALL_PREFIX}/lib/cmake/SharedLibConfig.cmake
+		#PATH_VARS ${CMAKE_BINARY_DIR@/bin}
+	)
+
+	EXPORT(TARGETS ${PackageName}
+		FILE ${PKGDIR}/${PackageName}Targets.cmake
+	)
+
 	MESSAGE(STATUS "RegPackage")
 	MESSAGE(STATUS "PackageName: " ${target})
 	MESSAGE(STATUS "PackageConfig_Dir: " ${PackageConfig_Dir})
 
-	SET (PKGDIR "${CMAKE_INSTALL_PREFIX}/${INSTALL_CONFIG_DIR}")
-	SET (PKGUID "${TOPLEVEL_PROJECT_PACKAGE_UID}")
+	#SET (PKGDIR "${CMAKE_INSTALL_PREFIX}/${INSTALL_CONFIG_DIR}")
+	#SET (PKGUID "${TOPLEVEL_PROJECT_PACKAGE_UID}")
+	#SET (PKGUID "aaaa")
+
+	MESSAGE(STATUS "CMAKE_CONFIGURATION_TYPES : " ${CMAKE_CONFIGURATION_TYPES})
+
+	#TODAY(RESULT)
+	#message(STATUS "Compilation date = ${RESULT}")
 
 	if (WIN32)
 		install (CODE
 		"execute_process (
-			COMMAND reg add \"HKCU\\\\Software\\\\Kitware\\\\CMake\\\\Packages\\\\${PackageName}\" /v \"${PKGUID}\" /d \"${PackageConfig_Dir}\" /t REG_SZ /f
+			COMMAND reg add \"HKCU\\\\Software\\\\Kitware\\\\CMake\\\\Packages\\\\${PackageName}\" /v \"${CMAKE_CONFIGURATION_TYPES}\" /d \"${PKGDIR}\" /t REG_SZ /f
 			RESULT_VARIABLE RT
 			ERROR_VARIABLE  ERR
 			OUTPUT_QUIET
 		)
 		if (RT EQUAL 0)
-			message (STATUS \"Register:   Added HKEY_CURRENT_USER\\\\Software\\\\Kitware\\\\CMake\\\\Packages\\\\${PackageName}\\\\${PKGUID}\")
+			message (STATUS \"Register:   Added HKEY_CURRENT_USER\\\\Software\\\\Kitware\\\\CMake\\\\Packages\\\\${PackageName}\\\\${CMAKE_CONFIGURATION_TYPES}\")
 		else ()
 			 string (STRIP \"\${ERR}\" ERR)
 			message (STATUS \"Register:   Failed to add registry entry: \${ERR}\")
 		endif ()"
 		)
 	elseif (IS_DIRECTORY "$ENV{HOME}")
-		file (WRITE "${BINARY_CONFIG_DIR}/${PackageName}RegistryFile" "${PKGDIR}")
+		file (WRITE "${BINARY_CONFIG_DIR}/${PackageName}RegistryFile" "${PackageConfig_Dir}")
 		install (
 			FILES       "${BINARY_CONFIG_DIR}/${PackageName}RegistryFile"
 			DESTINATION "$ENV{HOME}/.cmake/packages/${PackageName}"
-			RENAME      "${PKGUID}"
+			RENAME      "${CMAKE_CONFIGURATION_TYPES}"
 		)
 	endif ()
 
-	INCLUDE(CMakePackageConfigHelpers)
-
-	CONFIGURE_PACKAGE_CONFIG_FILE(
-	${CMAKE_SOURCE_DIR}/${PackageName}Config.cmake.in
-	${PackageConfig_Dir}/${PackageName}Config.cmake
-	INSTALL_DESTINATION ${CMAKE_INSTALL_PREFIX}/lib/cmake/SharedLibConfig.cmake
-	#PATH_VARS ${CMAKE_BINARY_DIR@/bin}
-)
-
-	EXPORT(TARGETS ${PackageName}
-		FILE ${PackageConfig_Dir}/${PackageName}Targets.cmake
-	)
+	
 ENDFUNCTION(RegPackage)
 
